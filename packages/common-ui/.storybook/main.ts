3d6c5b09db80be7787216e11ca4524f38cd6f3fb
namYeJi9q <nyj5850@naver.com>
2025-05-01 23:35:46 +0900

    [fix] storybook main.js edit

--- packages/common-ui/.storybook/main.ts
+++ packages/common-ui/.storybook/main.ts
@@ -1,17 +1,19 @@
-// .storybook/main.js
+// packages/ui/.storybook/main.js
+const path = require('path');
+
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  previewHead: "./.storybook/preview.tsx",
-  babel: async (options) => ({
-    ...options,
-    plugins: [
-      ...(options.plugins || []),
-      ["styled-components", { displayName: true }],
-    ],
-  }),
-  typescript: {
-    reactDocgen: "react-docgen-typescript",
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    // 필요시 절대 경로 설정 등
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@ui': path.resolve(__dirname, '../src'),
+    };
+    return config;
   },
 };
